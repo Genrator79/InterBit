17ce25085dd24ab0f4381539ab1d004cc01d85c0
Abhijeet Kumar <abhijeetkumar@Abhijeets-MacBook-Pro-3.local>
2025-12-31 14:26:44 +0530

    corrected the link for the dashboard link

--- frontend/src/app/interview/page.tsx
+++ frontend/src/app/interview/page.tsx
@@ -25,7 +25,7 @@ const page = () => {
       
       {/* Navbar */}
       <nav>
-        <Link href="/" className="flex items-center gap-2">
+        <Link href="/dashboard" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
           <h2 className="text-primary-100">InterBit</h2> 
         </Link>
